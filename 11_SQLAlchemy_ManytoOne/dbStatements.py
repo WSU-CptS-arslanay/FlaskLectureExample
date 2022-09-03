685a1f0ebed0ed6442755de959c0f1809e2b1bab
arslanay <arslanay@eecs.wsu.edu>
2022-09-03 00:35:59 -0700

    Adding versions 11, 12, 23, and 14.

--- 11_SQLAlchemy_ManytoOne/dbStatements.py
+++ 11_SQLAlchemy_ManytoOne/dbStatements.py
@@ -2,18 +2,6 @@
 from app import db
 from app.models import Course, Room
 db.create_all()
-# create Course and write it to DB
-newCourse = Course(major='CptS',coursenum='322', title='Software Engineering', roomid = 1)
-db.session.add(newCourse)
-newCourse = Course(major='CptS',coursenum='451', title='Database Systems', roomid = 1)
-db.session.add(newCourse)
-db.session.commit()
-
-Course.query.all()
-Course.query.filter_by(major='CptS').all()
-Course.query.filter_by(major='CptS').first()
-Course.query.filter_by(major='CptS').order_by(Course.title).all()
-Course.query.filter_by(major='CptS').count()
 
 # create Room and write it to DB
 newroom = Room(building='Sloan',roomNumber='175', capacity=100)
@@ -23,9 +11,25 @@ db.session.commit()
 # query the classes for a room
 theroom = Room.query.filter_by(id=1).first()
 theroom = Room.query.filter_by(building = 'Sloan').filter_by(roomNumber=175).first()
-# ???
+
+
+# create Course and write it to DB
+newCourse = Course(major='CptS',coursenum='322', title='Software Engineering', roomid = 1)
+db.session.add(newCourse)
+newCourse = Course(major='CptS',coursenum='451', title='Database Systems', roomid = theroom.id)
+db.session.add(newCourse)
+db.session.commit()
+
+Course.query.all()
+Course.query.filter_by(major='CptS').all()
+Course.query.filter_by(major='CptS').first()
+Course.query.filter_by(major='CptS').order_by(Course.title).all()
+Course.query.filter_by(major='CptS').count()
+
 
 # query the room for a class
 thecourse = Course.query.filter_by(major='CptS').filter_by(coursenum='322').first()
-## ???
+# will retrieve the room for the class
+thecourse.classroom
+
 
